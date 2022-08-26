39b5aa687248fab247cd1ef0dcf39b34f64594ba
Matias Gomez <you@example.com>
2022-08-25 22:55:01 -0300

    terminando visualizacion de Bienvenida

--- src/components/Bienvenida.js
+++ src/components/Bienvenida.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react'
 import { Navigate} from "react-router-dom";
-import "./Bienvenida.css"
+// import "./Bienvenida.css"
 import {Formik, Field} from 'formik'
 import axios from 'axios'
+import { BsGithub } from 'react-icons/bs';
+import { GrLinkedin } from 'react-icons/gr';
+import { BiErrorAlt } from 'react-icons/bi';
+
 
 
 
 const Bienvenida = () => {
-
+    // Capturamos los inputs en un estado
     const [body, setBody] = useState({ email: '', contraseña:'' })
     const [user, setUser] = useState({})
     
-
+    // Manejamos los cambios de los inputs
     const inputChange = ({target}) => {
         const { name, value } = target
         setBody({
@@ -20,6 +24,7 @@ const Bienvenida = () => {
         })
     };
 
+    // Enviamos info del login al backend
     const handleSubmit = async(e) => {
         e.preventDefault();
         let res = await axios.post('http://localhost:3001/api/users/login', {
@@ -32,32 +37,32 @@ const Bienvenida = () => {
 
 
   return (
-    <div className='page'>
-        <h1 className='saludo'>BIENVENIDO A TU NUEVA AGENDA DIGITAL</h1>
-        <div className='form_container'>
-            <form className='login'
+    <div className='relative bg-[url("https://img.freepik.com/fotos-premium/espacio-trabajo-plano-mano-mujer-taza-cafe-telefono-inteligente-computadora-cuaderno-agenda-papeleria_139507-102.jpg?w=2000")] bg-cover'>
+        
+        <div className='relative bg-blue-500 w-1/5 h-screen'>
+            <form className='grid grid-cols-1 grid-rows-4 p-5'
                 onSubmit={handleSubmit}
             >
-                <h3 className='login_title'>LOGIN</h3>
-                <div className='login_user'>
-                    <label>E-mail: </label>
-                    <input 
+                <h3 className='italic text-zinc-300 text-5xl font-bold tracking-wider p-2'>LOGIN</h3>
+                <div className='grid grid-cols-3 p-3'>
+                    <label className='text-lg italic font-medium'>E-mail: </label>
+                    <input className='col-span-2 border-2 border-black shadow-md'
                     type="text"
                     name='email' 
                     value={body.email}
                     onChange={inputChange}
                     />
                 </div>
-                <div className='login_password'>
-                    <label>Contraseña: </label>
-                    <input 
+                <div className='grid grid-cols-3 p-3'>
+                    <label className='text-lg italic font-medium'>Contraseña: </label>
+                    <input className='col-span-2 border-2 border-black shadow-md'
                     type="password" 
                     name='contraseña'
                     value={body.contraseña}
                     onChange={inputChange}
                     />
                 </div>
-                <button className='button_welcom' type='submit'>Login</button>
+                <button className='bg-black text-white rounded-2xl p-1 px-2 font-bold hover:bg-white hover:text-black ' type='submit'> Login </button>
             </form>
             <Formik
             // state de formik
@@ -74,11 +79,11 @@ const Bienvenida = () => {
                     let errores = {};
 
                     if(!valores.usuario){
-                        errores.usuario = 'Porfavor ingresa un nombre de usuario!.'
+                        errores.usuario = 'Porfavor ingresa un nombre de usuario!'
                     }
 
                     if(!valores.mail){
-                        errores.mail = 'Porfavor ingresa un correo de Email!.'
+                        errores.mail = 'Porfavor ingresa un correo de Email!'
                     } else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test (valores.mail)){
                         errores.mail = "El Email solo puede tener: letras, numeros, puntos, guines y guion bajo."
                     }
@@ -113,11 +118,11 @@ const Bienvenida = () => {
             >
                 {({values, touched, errors, handleSubmit, handleChange, handleBlur}) => (
 
-                <form className='register_form' onSubmit={handleSubmit}>
-                    <h3 className='register_title'>REGISTRATE </h3>
-                    <div className='register_user'>
-                        <label>Usuario: </label>
-                        <input 
+                <form className='grid grid-cols-1 grid-rows-4 p-5' onSubmit={handleSubmit}>
+                    <h3 className='italic text-zinc-300 text-5xl font-bold tracking-wider mt-10'>REGISTRATE </h3>
+                    <div className='pb-4'>
+                        <label className='italic font-medium text-lg block p-1'>Usuario </label>
+                        <input className='w-80 h-10 border-2 border-black shadow-md'
                             type="text" 
                             id='usuario' 
                             name='usuario'
@@ -125,11 +130,11 @@ const Bienvenida = () => {
                             onChange={handleChange}
                             onBlur={handleBlur}
                         />
-                        {touched.usuario && errors.usuario && <p className='error'>{errors.usuario}</p>}
+                        {touched.usuario && errors.usuario && <p className='w-fit p-1 mt-1 rounded-lg font-semibold text-white bg-red-500 '><BiErrorAlt className='w-5 h-5 mr-1 inline'/>{errors.usuario}</p>}
                     </div>
-                    <div className='register_mail'>
-                        <label>E-mail: </label>
-                        <input 
+                    <div className='pb-4'>
+                        <label className='italic font-medium text-lg block'>E-mail </label>
+                        <input className='w-80 h-10 border-2 border-black shadow-md'
                             type="email" 
                             id='mail' 
                             name='mail'
@@ -137,46 +142,48 @@ const Bienvenida = () => {
                             onChange={handleChange}
                             onBlur={handleBlur}
                         />
-                        {touched.mail && errors.mail && <p className='error'>{errors.mail}</p>}
+                        {touched.mail && errors.mail && <p className='w-fit p-1 mt-1 rounded-lg font-semibold text-white bg-red-500'><BiErrorAlt className='w-5 h-5 mr-1 inline'/>{errors.mail}</p>}
                     </div>
-                    <div className='register_password'>
-                        <label >Contraseña: </label>
-                        <input 
+                    <div className='pb-4'>
+                        <label className='italic font-medium text-lg block'>Contraseña </label>
+                        <input className='w-80 h-10 border-2 border-black shadow-md'
                             type="text" 
                             id='contraseña' 
                             name='contraseña'
                             onChange={handleChange}
                             onBlur={handleBlur}
                         />
-                        {touched.contraseña && errors.contraseña && <p className='error'>{errors.contraseña}</p>}
+                        {touched.contraseña && errors.contraseña && <p className='w-fit p-1 mt-1 rounded-lg font-semibold text-white bg-red-500'><BiErrorAlt className='w-5 h-5 inline mr-1'/>{errors.contraseña}</p>}
                     </div>
-                    <div className='register_confirm'>
-                        <label>Confirmar contraseña: </label>
-                        <input 
+                    <div className='pb-4'>
+                        <label className='italic font-medium text-lg block'>Confirmar contraseña </label>
+                        <input className='w-80 h-10 border-2 border-black shadow-md'
                             type="text" 
                             id='confirmar' 
                             name='confirmar'
                             onChange={handleChange}
                             onBlur={handleBlur}
                         />
-                        {touched.confirmar && errors.confirmar && <p className='error'>{errors.confirmar}</p>}
+                        {touched.confirmar && errors.confirmar && <p className='w-fit p-1 mt-1 rounded-lg font-semibold text-white bg-red-500'><BiErrorAlt className='w-5 h-5 mr-1 inline'/>{errors.confirmar}</p>}
                     </div>
-                    <button className='register_button' type='submit'>Registrarse</button>
+                    <button className='bg-black text-white rounded-2xl p-1 px-2 font-bold hover:bg-white hover:text-black ' type='submit'>Registrarse</button>
                 </form>
                 )}
             </Formik>
-            <h5 className='git'>GitHub: https://github.com/matuggiovagnoli</h5>
+            <div className='absolute bottom-0 left-0 inline'>
+                <h5 className='text-sm m-2'><BsGithub className='w-7 h-7 mr-2 inline'/>GitHub: https://github.com/matuggiovagnoli</h5>
+                <h5 className='text-sm m-2'><GrLinkedin className='w-6 h-6 mr-2 inline'/>Linkedin: www.linkedin.com/in/matias-daniel-gómez-giovagnoli-00b703177</h5>
+            </div>
         </div>
-        <div className='caracteristicas'>
-            <div className='contactos'>
-                <h3>Contactos</h3>
+        <div className='absolute flex left-1/3 bottom-0'>
+            <div className='m-10 h-60 w-80 opacity-20 hover:opacity-100 transition ease-in-out delay-150 hover:-translate-y-5 duration-500 hover:border-2 hover:border-black bg-[url("https://img.freepik.com/premium-photo/concept-contact-us-blank-wooden-cubes-square-dice-with-symbol-email-telephone-address-placed-wood-background_44868-1254.jpg?w=2000")] bg-cover bg-right'>
+                <h3 className='font-bold text-xl w-20'>CONTACTOS</h3>
             </div>
-            <image></image>
-            <div className='calendario'>
-                <h3>Calendario</h3>
+            <div className='m-10 h-60 w-80 opacity-20 hover:opacity-100 transition ease-in-out delay-150 hover:-translate-y-5 duration-500 hover:border-2 hover:border-black bg-[url("https://thumbs.dreamstime.com/b/red-pin-event-calendar-background-close-up-time-red-pin-event-calendar-background-close-up-time-149540392.jpg")] bg-cover'>
+                <h3 className='font-bold  text-xl w-20'>CALENDARIO</h3>
             </div>
-            <div className='posts'>
-                <h3>Post It</h3>
+            <div className='m-10 h-60 w-80 opacity-20 hover:opacity-100 transition ease-in-out delay-150 hover:-translate-y-5 duration-500 hover:border-2 hover:border-black bg-[url("https://images.unsplash.com/photo-1578450671530-5b6a7c9f32a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cG9zdCUyMGl0fGVufDB8fDB8fA%3D%3D&w=1000&q=80")] bg-cover'>
+                <h3 className='font-bold  text-xl w-20'>POST IT</h3>
             </div>
         </div>
     </div>
